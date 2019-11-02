b5653d6651aa1e7f29461fe4e3dd7614c77b85d5
Your Name <khorenterhovhannisyan@gmail.com>
2019-11-02 21:09:42 +0400

    fixed bug

--- src/Script/login.js
+++ src/Script/login.js
@@ -4,39 +4,9 @@ window.onload = () => {
     }
 }
 
-const firstname = document.getElementById('reg_firstName');
-const lastname = document.getElementById('reg_lastName');
-const password = document.getElementById('password');
-const email = document.getElementById('email');
-const city = document.getElementById('city');
+
 const urlReg = 'https://it-blog-posts.herokuapp.com/api/people'
 
-const regFetch = () => {
-    let data = {
-        lastname: lastname.value,
-        firstname: firstname.value,
-        password: password.value,
-        email: email.value,
-        city: city.value
-    }
-    console.log(data);
-
-    fetch(`${urlReg}`,
-        {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then((data) => {window.open('Login.html');
-            return data;
-        })
-        .catch(function (err) {
-            console.log("Error", err);
-        })
-}
 
 const loginFetch = () => {
     let data = {
